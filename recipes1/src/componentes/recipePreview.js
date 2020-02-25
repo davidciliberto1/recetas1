17fdf321065b416b9f4362a6b0573e8668ad69d1
molivella <molivella@finnegans.com.ar>
2020-02-25 19:45:34 -0300

    css

--- recipes1/src/componentes/recipePreview.js
+++ recipes1/src/componentes/recipePreview.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const RecipePreview = ({onButtonClick, recipe }) => {
     return (
-        <div>
-            <img height='50px' src={recipe.recipe.image} alt="" />
+        <div className='Preview'>
+            <img className='imagen' height='50px' src={recipe.recipe.image} alt="" />
             <button onClick={(event) => onButtonClick(event, recipe)} >{recipe.recipe.label}</button>
             
             

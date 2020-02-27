89b478968702f88441cd92be2376277f74a4c0d2
deivid <davidciliberto@gmail.com>
2020-02-26 21:43:34 -0300

    buttons

--- recipes1/src/componentes/recipePreview.js
+++ recipes1/src/componentes/recipePreview.js
@@ -5,7 +5,7 @@ const RecipePreview = ({onButtonClick, recipe }) => {
     return (
         <div className='Preview'>
             <img className='imagen' height='50px' src={recipe.recipe.image} alt="" />
-            <button onClick={(event) => onButtonClick(event, recipe)} >{recipe.recipe.label}</button>
+            <button className='button btn1' onClick={(event) => onButtonClick(event, recipe)} >{recipe.recipe.label}</button>
             
             
         </div>
